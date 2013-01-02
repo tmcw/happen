72434d08f487ead01c2b4077c9f45a3b10178db5
Tom MacWright <tom@macwright.org>
2013-01-02 18:25:21 -0500

    Polyfill object create

--- test/chai.js
+++ test/chai.js
@@ -10,6 +10,17 @@
   }
 }('chai', this, function () {
 
+  if (!Object.create) {
+      Object.create = function (o) {
+          if (arguments.length > 1) {
+              throw new Error('Object.create implementation only accepts the first parameter.');
+          }
+          function F() {}
+          F.prototype = o;
+          return new F();
+      };
+  }
+
   function require(p) {
     var path = require.resolve(p)
       , mod = require.modules[path];
